747534c4d69e04918c0c1a0d0ea96ec96cc199f1
khiandraj <kjoh6987@students.vsu.edu>
2025-09-25 11:30:05 -0400

    Ping method

--- MongoDBConnector/MongoDBConnector.cs
+++ MongoDBConnector/MongoDBConnector.cs
@@ -1,6 +1,27 @@
-﻿namespace MongoDBConnector;
+﻿using MongoDB.Driver;
+
+namespace MongoDBConnector;
 
 public class Class1
 {
 
+    private readonly MongoClient_client; 
+
+    public MongoDBConnector (string connectionString){
+        _client = new MongoClient(connectionString); 
+    }
+}
+public bool Ping()
+{
+    try
+    {
+        var database = _client.GetDatabase("admin");
+        var command = new BsonDocument("ping", 1);
+        database.RunCommand<BsonDocument>(command);
+        return true;
+    }
+    catch
+    {
+        return false;
+    }
 }
